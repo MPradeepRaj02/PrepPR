e86b5da515623173d60e475bfd23af4cf62a79a0
Pradeep Raj <mpradeepraj004mpr@gmail.com>
2022-08-30 22:49:32 +0530

    Answers

--- Find the length of a string without using library function.c
+++ Find the length of a string without using library function.c
@@ -1,1 +1,14 @@
-//Answer will be updated shortly
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    fgets(str, sizeof str, stdin);
+
+    while(str[len] != '\0') { len++; }
+    printf("Length of String: %d",len-1);
+
+    return 0;
+}
--- Separate the individual characters from a string.c
+++ Separate the individual characters from a string.c
@@ -1,1 +1,14 @@
-//Answer will be updated shortly
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[100];
+
+    fgets(str, sizeof str, stdin);
+
+    for(int i=0; str[i] != '\0'; i++){
+        printf("%c ",str[i]);
+    }
+
+    return 0;
+}
